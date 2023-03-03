3bc74df458f0285031909b0f8cd4440af136234d
Brody14 <stefania.spinas@gmail.com>
2023-03-03 16:36:11 +0100

    FEAT card with props

---
+++ src/components/Card.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="card">
+        <img class="card-img" :src="imgSrc" alt="">
+        <div class="card-body">
+            <h2 class="card_name">{{ card.name }}</h2>
+            <p class="card_archetype"> {{ card.archetype }}</p>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        card: {
+            type: Object,
+            required: true
+        },
+        imgSrc: {
+            type: String,
+            required: true
+        }
+
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+
+.card {
+    text-transform: uppercase;
+    font-size: 8px;
+
+    .card-body {
+        background-color: $bg-color;
+        text-align: center;
+        padding: 20px;
+        height: 100px;
+
+        .card_name {
+            color: white;
+            margin-bottom: 10px;
+        }
+
+        .card_archetype {
+            font-size: 15px;
+            text-transform: capitalize;
+        }
+    }
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,30 @@
 <script>
-export default {
+import axios from 'axios'
+import Card from './Card.vue'
 
+export default {
+    components: {
+        Card,
+    },
+    data() {
+        return {
+            cards: [],
+        }
+    },
+    methods: {
+        fetchCards() {
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+                .then((res) => {
+                    //console.log(res, res.data, res.data.data)
+                    const card = res.data.data
+                    this.cards = card
+                    //console.log(card.card_images)
+                })
+        }
+    },
+    mounted() {
+        this.fetchCards()
+    }
 }
 </script>
 
@@ -8,7 +32,7 @@ export default {
     <main class="main-content">
         <div class="container">
             <div class="grid">
-
+                <Card v-for="card in cards" :key="card.id" :card="card" :imgSrc="card.card_images[0].image_url" />
             </div>
 
         </div>
@@ -20,16 +44,18 @@ export default {
 
 .main-content {
     background-color: $bg-color;
-    padding: 20px;
+    padding: 60px;
 
     .container {
         background-color: white;
         padding: 20px;
+        justify-content: center;
     }
 }
 
 .grid {
     display: grid;
     grid-template-columns: repeat(5, 1fr);
+    gap: 30px
 }
 </style>
