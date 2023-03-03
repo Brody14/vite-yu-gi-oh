ccedbc91387114ffba3c8feeb273fb37fe8daee8
Brody14 <stefania.spinas@gmail.com>
2023-03-03 14:48:45 +0100

    FEAT header

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <header class="main-header">
+        <div class="logo-wrapper">
+            <img class="logo" src="/img/logo.webp" alt="">
+            <h1>Yu-Ghi-Oh Api</h1>
+        </div>
+
+    </header>
+</template>
+
+<style lang="scss" >
+.logo-wrapper {
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+
+    .logo {
+        width: 70px;
+    }
+}
+</style>
